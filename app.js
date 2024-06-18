31d31a5921485371100894a7b54fca0960f6cffc
Ellyda Micaella <ellydamicaella810@gmail.com>
2024-06-18 10:53:00 -0300

    commitando js

--- app.js
+++ app.js
@@ -2,16 +2,15 @@
     let numeroSecreto  = 29;
     let tentativas = 1;
 
-
-
-
     while(palpite != numeroSecreto) {
        
         let palpite =parseInt(prompt("Escolha um numero entre 1 e 30"));
 
     // confere se o palpite é igual ao numero secreto e se não é, ajuda dando dica
         if ( palpite == numeroSecreto ){
-            alert(`vc acertou o numero secreto era ${numeroSecreto} com ${tentativas}.`);
+
+        break;
+
         } else {
             if (palpite < numeroSecreto){
                 alert(`O número secreto é maior que ${palpite}`);
@@ -23,3 +22,8 @@
         }   
     }
 
+    if(tentativas < 1){
+    alert(`vc acertou! o numero secreto era ${numeroSecreto} com ${tentativas} tentativas.`);
+    } else{
+    alert(`vc acertou! O numero secreto era ${numeroSecreto} com ${tentativas} tentativa`);
+    }
