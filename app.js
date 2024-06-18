a1907d6622bb645ccd5ae2360cb3a7264fd0024e
Ellyda Micaella <ellydamicaella810@gmail.com>
2024-06-18 18:50:33 -0300

    adicionando funcionalidades no app.js

--- app.js
+++ app.js
@@ -1,29 +1,39 @@
-    alert("Boas vindas ao meu jogo de adivinhação");
-    let numeroSecreto  = 29;
-    let tentativas = 1;
+alert("Boas vindas ao meu jogo de adivinhação");
+let valorMaximo = 1000;
+let numeroSecreto  = parseInt(Math.random()* valorMaximo + 1);
+console.log(numeroSecreto);
+let palpite ;
+let tentativas = 1;
 
-    while(palpite != numeroSecreto) {
-       
-        let palpite =parseInt(prompt("Escolha um numero entre 1 e 30"));
 
-    // confere se o palpite é igual ao numero secreto e se não é, ajuda dando dica
-        if ( palpite == numeroSecreto ){
+while(palpite != numeroSecreto) {
+   
+   palpite =parseInt(prompt(`Escolha um numero entre 1 e ${valorMaximo}`));
 
+// confere se o palpite é igual ao numero secreto e se não é, ajuda dando dica
+    if ( palpite == numeroSecreto ){
         break;
 
+    } else {
+        
+        if (palpite < numeroSecreto){
+            alert(`O número secreto é maior que ${palpite}`);
         } else {
-            if (palpite < numeroSecreto){
-                alert(`O número secreto é maior que ${palpite}`);
-            } else {
-                alert(`O número secreto é menor que ${palpite}`);
-            }
-            //tentativas = tentativas + 1;
-            tentativas ++
-        }   
-    }
+            alert(`O número secreto é menor que ${palpite}`);
+        }
+        //tentativas = tentativas + 1;
+        tentativas ++
+    }   
+}
+//substituindo esse codigo abaixo por uma forma mais adwquada chamada operador tenario
+/*if(tentativas < 1){
+alert(`vc acertou! o numero secreto era ${numeroSecreto} com ${tentativas} tentativas.`);
+} else{
+alert(`vc acertou! O numero secreto era ${numeroSecreto} com ${tentativas} tentativa`);
+}*/
 
-    if(tentativas < 1){
-    alert(`vc acertou! o numero secreto era ${numeroSecreto} com ${tentativas} tentativas.`);
-    } else{
-    alert(`vc acertou! O numero secreto era ${numeroSecreto} com ${tentativas} tentativa`);
-    }
+// lê-se da seguinte forma 
+//tentativa maior que 1? Caso seja escreva 'tentativas' se não 'tentativa'
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
+
+alert(`vc acertou! O numero secreto era ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
